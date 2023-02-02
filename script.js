7cb3381cbcd853aaf464c18598f255696c362529
Dewald Fourie <dewaldfourie0808@gmail.com>
2023-02-02 15:51:11 +0200

    added comments

--- script.js
+++ script.js
@@ -2,14 +2,17 @@
 let color = 'black';
 let click = true;
 
-
+// Function to add a board to the container taking the input sie of user
+// Default = 32x32
 function populateBoard (size) {
+    // clears the board if there is previous configuration
     let board = document.querySelector('.board');
     let squares = board.querySelectorAll('div');
     squares.forEach((div) => div.remove());
+    // creates the columns and rows with the size input
     board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
+    // creates div elements for the area of the board with size
     for( i = 0; i < (size*size); i++){
         let square = document.createElement('div');
         square.addEventListener('mouseover', colorSquare)
@@ -18,7 +21,7 @@ function populateBoard (size) {
     }
 }
 
-
+// change the size of the board using above function with parameters 2 and 100
 function changeSize(input){
     if ( input >= 2 && input < 100){
         document.querySelector('.error').style.display = 'none'
@@ -30,6 +33,7 @@ function changeSize(input){
     }
 }
 
+// function to add color to the square where mouse is pointed
 function colorSquare() {
     if (click){
         if (color === 'random'){
@@ -38,23 +42,22 @@ function colorSquare() {
         else {
             this.style.backgroundColor = color;
         }
-    }
-    else {
+    }   
+}
 
-    }
-      
-    }
-
+// Function to change the color of the pointer by listening to buttons in html
 function changeColor(choice) {
     color = choice;
 }
 
+// Function to reset the board, basically turns all the squares back to white, erases all.
 function resetBoard(){
     let board = document.querySelector('.board');
     let squares = board.querySelectorAll('div');
     squares.forEach((div) => div.style.backgroundColor = 'white');
 }
 
+// determine if miouse is clicked = true of false to show if coloring or not. UX helper
 document.querySelector('body').addEventListener('click', (e) => {
     if (e.target.tagName != 'BUTTON'){
         click = !click;
